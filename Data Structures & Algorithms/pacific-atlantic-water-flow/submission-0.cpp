e720c298f76f3bb0c6c97f433277262247054a90
Jayesh Agarwal <138282025+JayeshAgarwal03@users.noreply.github.com>
2026-07-10 21:09:47 +0530

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &heights, vector<vector<int>> &ocean, int i, int j){
+        ocean[i][j]=1;
+
+        int dx[4]={-1, 0, 1, 0};
+        int dy[4]={0, 1, 0, -1};
+
+        for(int p=0; p<4; p++){
+            if(i+dx[p]>=0 && i+dx[p]<heights.size() && j+dy[p]>=0 && j+dy[p]<heights[0].size()){
+                if(!ocean[i+dx[p]][j+dy[p]] && heights[i+dx[p]][j+dy[p]]>=heights[i][j]){
+                    cout<<i+dx[p]<<" "<<j+dy[p]<<"\n";
+                    dfs(heights, ocean, i+dx[p], j+dy[p]);
+                }
+            }
+        }
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        vector<vector<int>> pacific(heights.size(), vector<int>(heights[0].size(), 0));
+        vector<vector<int>> atlantic(heights.size(), vector<int>(heights[0].size(), 0));
+
+        for(int j=0; j<heights[0].size(); j++){
+            if(!pacific[0][j]){
+                dfs(heights, pacific, 0, j);
+            }
+        }
+        for(int i=1; i<heights.size(); i++){
+            if(!pacific[i][0]){
+                dfs(heights, pacific, i, 0);
+            }
+        }
+
+        for(int j=0; j<heights[0].size(); j++){
+            if(!atlantic[heights.size()-1][j]){
+                dfs(heights, atlantic, heights.size()-1, j);
+            }
+        }
+        for(int i=0; i<heights.size(); i++){
+            if(!atlantic[i][heights[0].size()-1]){
+                dfs(heights, atlantic, i, heights[0].size()-1);
+            }
+        }
+        
+        vector<vector<int>> ans;
+        for(int i=0; i<heights.size(); i++){
+            for(int j=0; j<heights[0].size(); j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    ans.push_back({i, j});
+                }
+            }
+        }
+
+        for(int i=0; i<heights.size(); i++){
+            for(int j=0; j<heights[0].size(); j++){
+                cout<<pacific[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+
+        for(int i=0; i<heights.size(); i++){
+            for(int j=0; j<heights[0].size(); j++){
+                cout<<atlantic[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+
+        return ans;
+    }
+};
