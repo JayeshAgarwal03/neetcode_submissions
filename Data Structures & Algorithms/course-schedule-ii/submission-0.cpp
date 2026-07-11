a77e2ce377d6b99a9612101f1ac47e0c461bd1bb
Jayesh Agarwal <138282025+JayeshAgarwal03@users.noreply.github.com>
2026-07-12 04:16:42 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void topo_bfs(vector<vector<int>> &adj, vector<int> &indegrees, queue<int> &q, vector<int> &topo){
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it:adj[node]){
+                indegrees[it]--;
+                if(indegrees[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegrees(numCourses, 0);
+
+        for(auto it:prerequisites){
+            adj[it[0]].push_back(it[1]);
+            indegrees[it[1]]++;
+        }
+        
+        queue<int> q;
+
+        for(int i=0; i<indegrees.size(); i++){
+            if(indegrees[i]==0){
+                q.push(i);
+            }
+        }
+        
+        vector<int> topo;
+        topo_bfs(adj, indegrees, q, topo);
+
+        if(topo.size()==numCourses){
+            reverse(topo.begin(), topo.end());
+            return topo;
+        }
+        else{
+            return {};
+        }
+        
+    }
+};
